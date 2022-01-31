d259d7df746b8d8c4cea8d042195a9c5969b82bc
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-02-01 00:30:34 +0530

    Add files via upload

---
+++ Data Structure and Algorithms/src/leetcodeDaily/KokoEatingBanana.java
@@ -0,0 +1,41 @@
+package leetcodeDaily;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class KokoEatingBanana {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter size of the array - ");
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0; i<n;i++) {
+			a[i]=sc.nextInt();	
+		}
+		System.out.println(Arrays.toString(a));
+		System.out.print("The the number ofhour needed : ");
+		int x=sc.nextInt();
+		System.out.println("output is "+ minEatingSpeed(a,x));	
+
+
+	}
+	
+	 public static int minEatingSpeed(int[] piles, int h) {
+		 int start=1;
+	        int end=1000000000;
+	        while(start<=end){
+	            int mid=start+(end-start)/2;
+	            int newH=0;
+	            for(int n:piles){
+	               newH+=n/mid;
+	               if(n%mid!=0) newH++;
+	            }
+	        if(newH>h)
+	            start=mid+1;
+	            
+	        if(newH<=h)
+	            end=mid-1;
+	        }
+	        return start;
+	    }
+}
