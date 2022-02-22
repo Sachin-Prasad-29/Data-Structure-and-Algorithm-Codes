95b0315c0f01242e86a95e7c445e27f28a41c405
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-02-22 23:59:20 +0530

    new code

---
+++ Data Structure and Algorithms/src/Test_Questions/multiply.java
@@ -0,0 +1,12 @@
+package Test_Questions;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 60;
+        for(int i=0 ; i <10 ; i++){
+            System.out.println(a*b*789*i);
+        }
+    }
+    
+}
