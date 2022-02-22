333117b6a0bcebac2509f8ea6f432961d81978c0
Sachin kumar prasad <39449370+Sachin-Prasad-29@users.noreply.github.com>
2022-02-22 23:43:25 +0530

    add

---
+++ Data Structure and Algorithms/src/Test_Questions/Additon.java
@@ -0,0 +1,13 @@
+package Test_Questions;
+
+public class Additon {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		for(int i= 0;i<5;i++)
+		 System.out.println(i*a+b);
+
+	}
+
+}
